0928e3c2ef04a4ea9a9007f84fcfbbc03d18f257
Nahor-irup <rohanpuri195@gmail.com>
2022-08-03 22:38:56 +0545

    Exercise 1.11 completed

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -9,7 +9,16 @@ const Button = (props) => {
 const StatisticLine = (props) => {
   const { text, value } = props
   return (
-    <div>{text} {value}</div>
+    <div>
+      <table border="1px solid" width="20%">
+        <tbody>
+          <tr>
+            <td align='left'>{text}</td>
+            <td align='right'>{value}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div >
   )
 }
 
@@ -27,7 +36,7 @@ const Statistics = (props) => {
         <StatisticLine text={"neutral"} value={neutral} />
         <StatisticLine text={"bad"} value={bad} />
         <StatisticLine text={"all"} value={good + neutral + bad} />
-        <StatisticLine text={"average"} value={(good + neutral * 0 + bad * -1) / (good + neutral + bad)} />
+        <StatisticLine text={"average"} value={(good - bad) / (good + neutral + bad)} />
         <StatisticLine text={"positive"} value={good / (good + neutral + bad) * 100 + " %"} />
       </div>
     )
